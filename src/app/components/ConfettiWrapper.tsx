b2d39e312a0a23edfada9adeb958bc9905a1fea0
Ezekiel Faith <ezekielfaith2001@gmail.com>
2025-07-20 13:53:20 +0100

    added more theme

--- src/app/components/ConfettiWrapper.tsx
+++ src/app/components/ConfettiWrapper.tsx
@@ -1,4 +1,3 @@
-// src/components/ConfettiWrapper.tsx
 "use client";
 
 import { useState, useEffect } from 'react';
--- src/config/themes.ts
+++ src/config/themes.ts
@@ -19,6 +19,43 @@ export const themes: Theme[] = [
   {
     name: 'Forest',
     id: 'forest',
+    karmaRequired: 300,
+  },
+    {
+    name: 'Sunset',
+    id: 'sunset',
     karmaRequired: 500,
   },
+    {
+    name: 'Aqua',
+    id: 'aqua',
+    karmaRequired: 700,
+  },
+    {
+    name: 'Retro',
+    id: 'retro',
+    karmaRequired: 900,
+  },
+    {
+    name: 'BubbleGum',
+    id: 'bubblegum',
+    karmaRequired: 1000,
+  },
+  {
+    name: 'Minty',
+    id: 'minty',
+    karmaRequired: 1200,
+  },
+    {
+    name: 'GrapeBerry',
+    id: 'grapeberry',
+    karmaRequired: 1300,
+  },
+
+    {
+    name: 'PastelDream',
+    id: 'pasteldream',
+    karmaRequired: 1500,
+  },
+
 ];
